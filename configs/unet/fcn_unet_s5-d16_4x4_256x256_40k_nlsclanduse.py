9b12b0116b006c4257a64072c58bec43f8584d75
code14 <mob5566@gmail.com>
2022-03-06 17:57:38 +0800

    [Feature] fix unet config for NLSC Landuse
    Signed-off-by: code14 <mob5566@gmail.com>

--- configs/unet/fcn_unet_s5-d16_4x4_256x256_40k_nlsclanduse.py
+++ configs/unet/fcn_unet_s5-d16_4x4_256x256_40k_nlsclanduse.py
@@ -6,6 +6,9 @@ _base_ = [
 model = dict(
     decode_head=dict(num_classes=9),
     auxiliary_head=dict(num_classes=9)
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole')
 )
 
 data = dict(
