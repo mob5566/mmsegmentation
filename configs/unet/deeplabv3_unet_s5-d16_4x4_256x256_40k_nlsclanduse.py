733220d895f1846026bbabe78be94a3c54b580b6
code14 <mob5566@gmail.com>
2022-03-20 15:36:09 +0800

    [Feature] add DeepLabV3 /w UNet backbone for NLSC Landuse dataset

---
+++ configs/unet/deeplabv3_unet_s5-d16_4x4_256x256_40k_nlsclanduse.py
@@ -0,0 +1,24 @@
+_base_ = [
+    '../_base_/models/deeplabv3_unet_s5-d16.py',
+    '../_base_/datasets/nlsclanduse.py', '../_base_/default_runtime.py',
+    '../_base_/schedules/schedule_40k.py'
+]
+
+model = dict(
+    decode_head=dict(num_classes=8),
+    auxiliary_head=dict(num_classes=8),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
+
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=4,
+)
+
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook', by_epoch=False),
+        dict(type='TensorboardLoggerHook')
+    ])
