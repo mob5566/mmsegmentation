39067d7e9b839bbe74ce0cd182d7d8ee352714c0
code14 <mob5566@gmail.com>
2022-05-16 01:34:13 +0800

    [Feature] add DA config for NLSC Landuse dataset
    Signed-off-by: code14 <mob5566@gmail.com>

---
+++ configs/_base_/models/daformer_sepaspp_mitb5_nlsclanduse.py
@@ -0,0 +1,15 @@
+_base_ = ['daformer_conv1_mitb5.py']
+
+norm_cfg = dict(type='BN', requires_grad=True)
+model = dict(
+    decode_head=dict(
+        num_classes=8,
+        decoder_params=dict(
+            fusion_cfg=dict(
+                _delete_=True,
+                type='aspp',
+                sep=True,
+                dilations=(1, 6, 12, 18),
+                pool=False,
+                act_cfg=dict(type='ReLU'),
+                norm_cfg=norm_cfg))))
---
+++ tools/convert_datasets/nlsclanduse.py
@@ -0,0 +1,131 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import argparse
+import json
+import os.path as osp
+
+import mmcv
+import numpy as np
+from PIL import Image
+
+ID_TABLE = dict({
+    8:
+    dict({
+        0: 0,
+        1: 1,
+        2: 1,
+        3: 1,
+        4: 2,
+        5: 2,
+        6: 2,
+        7: 3,
+        8: 4,
+        9: 5,
+        10: 6,
+    }),
+    11:
+    dict({
+        0: 0,
+        1: 1,
+        2: 2,
+        3: 3,
+        4: 4,
+        5: 5,
+        6: 6,
+        7: 7,
+        8: 8,
+        9: 9,
+        10: 10,
+    })
+})
+
+
+def convert_to_train_id(args):
+    file, n_class = args
+    # re-assign labels to match the format of Cityscapes
+    pil_label = Image.open(file)
+    label = np.asarray(pil_label)
+    id_to_trainid = ID_TABLE[n_class]
+    label_copy = np.zeros(label.shape, dtype=np.uint8)
+    sample_class_stats = {}
+    for k, v in id_to_trainid.items():
+        k_mask = label == k
+        label_copy[k_mask] = v
+        n = int(np.sum(k_mask))
+        if n > 0:
+            sample_class_stats[v] = n
+    new_file = file.replace('.png', f'_labelTrainIds{n_class}cls.png')
+    assert file != new_file
+    sample_class_stats['file'] = new_file
+    Image.fromarray(label_copy, mode='L').save(new_file)
+    return sample_class_stats
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Convert NLSC landuse annotations to TrainIds')
+    parser.add_argument('nlsc_path', help='nlsc data path')
+    parser.add_argument('--gt-dir', default='labels', type=str)
+    parser.add_argument('--num-cls', default=8, type=int, choices=(8, 11))
+    parser.add_argument('-o', '--out-dir', help='output path')
+    parser.add_argument(
+        '--nproc', default=4, type=int, help='number of process')
+    args = parser.parse_args()
+    return args
+
+
+def save_class_stats(out_dir, sample_class_stats):
+    with open(osp.join(out_dir, 'sample_class_stats.json'), 'w') as of:
+        json.dump(sample_class_stats, of, indent=2)
+
+    sample_class_stats_dict = {}
+    for stats in sample_class_stats:
+        f = stats.pop('file')
+        sample_class_stats_dict[f] = stats
+    with open(osp.join(out_dir, 'sample_class_stats_dict.json'), 'w') as of:
+        json.dump(sample_class_stats_dict, of, indent=2)
+
+    samples_with_class = {}
+    for file, stats in sample_class_stats_dict.items():
+        for c, n in stats.items():
+            if c not in samples_with_class:
+                samples_with_class[c] = [(file, n)]
+            else:
+                samples_with_class[c].append((file, n))
+    with open(osp.join(out_dir, 'samples_with_class.json'), 'w') as of:
+        json.dump(samples_with_class, of, indent=2)
+
+
+def main():
+    args = parse_args()
+    nlsc_path = args.nlsc_path
+    out_dir = args.out_dir if args.out_dir else nlsc_path
+    mmcv.mkdir_or_exist(out_dir)
+
+    gt_dir = osp.join(nlsc_path, args.gt_dir)
+
+    poly_files = []
+    for poly in mmcv.scandir(
+            gt_dir, suffix=tuple(f'{i}.png' for i in range(10)),
+            recursive=True):
+        poly_file = osp.join(gt_dir, poly)
+        poly_files.append(poly_file)
+    poly_files = sorted(poly_files)
+
+    paras = list(zip(poly_files, [args.num_cls] * len(poly_files)))
+    only_postprocessing = False
+    if not only_postprocessing:
+        if args.nproc > 1:
+            sample_class_stats = mmcv.track_parallel_progress(
+                convert_to_train_id, paras, args.nproc)
+        else:
+            sample_class_stats = mmcv.track_progress(convert_to_train_id,
+                                                     paras)
+    else:
+        with open(osp.join(out_dir, 'sample_class_stats.json'), 'r') as of:
+            sample_class_stats = json.load(of)
+
+    save_class_stats(out_dir, sample_class_stats)
+
+
+if __name__ == '__main__':
+    main()
